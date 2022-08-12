c4150730e5f65042d2c8c357dfc0320779939bd6
Amir mohammad <am.sanei79@gmail.com>
2022-08-13 02:28:41 +0430

    Add some dummy data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Flower from "./pages/Flower";
+import Checkout from "./pages/Checkout";
+import Cart from "./pages/Cart";
 function App() {
-  return (
-    <h1>Flowers 🌸</h1>
-  );
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/flower" element={<Flower />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route path="/cart" element={<Cart />} />
+                </Routes>
+            </Router>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/Flower.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Flower extends Component {
+    render() {
+        return <div>Flower</div>;
+    }
+}
