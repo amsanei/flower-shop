343c26f8c4d21da68781da030701ed03a759f9b6
Amir mohammad <am.sanei79@gmail.com>
2022-08-13 02:45:03 +0430

    Add FlowersList component for showing all flowers

---
+++ src/components/FlowersList.jsx
@@ -0,0 +1,47 @@
+export default function FlowersList() {
+    const flowers = [
+        {
+            id: "ag78uy",
+            imgUrl: "notFound",
+            name: "Sun Flower",
+            Price: 2000,
+            inStock: 5,
+            description:
+                "Description is the pattern of narrative development that aims to make vivid a place, object, character, or group.",
+        },
+        {
+            id: "ie96qo",
+            imgUrl: "notFound",
+            name: "Daisy",
+            Price: 1000,
+            inStock: 5,
+            description:
+                "Description is the pattern of narrative development that aims to make vivid a place, object, character, or group.",
+        },
+        {
+            id: "agww32",
+            imgUrl: "notFound",
+            name: "Rose",
+            Price: 2000,
+            inStock: 8,
+            description:
+                "Description is the pattern of narrative development that aims to make vivid a place, object, character, or group.",
+        },
+    ];
+
+    if (!flowers) {
+        return <div>Not flower found</div>;
+    }
+
+    return (
+        <main>
+            {flowers.map((flower) => (
+                <div className="flower">
+                    <h1 className="flower-title">{flower.name}</h1>
+                    <p className="flower-desc">{flower.description}</p>
+                    <div className="flower-price">{flower.Price}</div>
+                </div>
+            ))}
+        </main>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Header from './../components/Header';
+import FlowersList from './../components/FlowersList';
 
 
 export default class Home extends Component {
     render() {
         return <div>
             <Header />
+            <FlowersList/>
         </div>;
     }
 }
