d3c4cae8694714a9b5f09f1368caab022381dcec
Amir mohammad <am.sanei79@gmail.com>
2022-08-22 16:02:31 +0430

    Create UserContext.js

---
+++ src/context/UserContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    const [users, setUsers] = useState([
+        {
+            id: "1a2a1a",
+            username: "amir",
+            password: "amir12345",
+            email: "a@a,ir",
+        },
+    ]);
+
+    return (
+        <UserContext.Provider value={users}>{children}</UserContext.Provider>
+    );
+};
