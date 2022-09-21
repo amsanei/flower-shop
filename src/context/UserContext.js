011ab10c5147d34538d8e887704e19a23f45895b
sanei <am.sanei79@gmail.com>
2022-09-21 05:36:29 +0430

    Add user information editing functionality

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -4,25 +4,25 @@ import { v4 as uuidv4 } from "uuid";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-    const defaultUser = {
-        id: "1a2a1a",
-        username: "amir",
-        password: "amir12345",
-        fullName: "amir sanei",
-        gender: "male",
-        email: "am.sanei79@gmail.com",
-        phoneNumber: "09147895462",
-        state: "ardebil",
-        city: "germi",
-        zipCode: 123456789,
-        address: "First street , aa block , red door",
-        imageUrl:
-            "https://images.unsplash.com/photo-1519689373023-dd07c7988603?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-    };
-    const [users, setUsers] = useState([defaultUser]);
-
-    const [loggedinUser, setLoggedinUser] = useState(defaultUser);
-    const [isLoggedin, setIsLoggedin] = useState(true);
+    const [users, setUsers] = useState([
+        {
+            id: "1a2a1a",
+            username: "amir",
+            password: "amir12345",
+            fullName: "amir sanei",
+            gender: "male",
+            email: "am.sanei79@gmail.com",
+            phoneNumber: "09147895462",
+            state: "ardebil",
+            city: "germi",
+            zipCode: "123456789",
+            address: "First street , aa block , red door",
+            imageUrl:
+                "https://images.unsplash.com/photo-1519689373023-dd07c7988603?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        },
+    ]);
+    const [loggedinUser, setLoggedinUser] = useState({});
+    const [isLoggedin, setIsLoggedin] = useState(false);
 
     const userLogin = (userId) => {
         setLoggedinUser(users.find((user) => user.id === userId));
@@ -46,6 +46,21 @@ export const UserProvider = ({ children }) => {
         ]);
     };
 
+    const editUser = (newInfo) => {
+        const newUserInfo = {
+            id: loggedinUser.id,
+            password: loggedinUser.password,
+            imageUrl: loggedinUser.imageUrl,
+            ...newInfo,
+        };
+        setLoggedinUser(newUserInfo);
+        setUsers(
+            users.map((user) =>
+                user.id === loggedinUser.id ? newUserInfo : user
+            )
+        );
+    };
+
     return (
         <UserContext.Provider
             value={{
@@ -55,6 +70,7 @@ export const UserProvider = ({ children }) => {
                 isLoggedin,
                 userLogout,
                 addUser,
+                editUser,
             }}
         >
             {children}
--- src/pages/userPanel/UserInformation.jsx
+++ src/pages/userPanel/UserInformation.jsx
@@ -1,10 +1,46 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import UserContext from "../../context/UserContext";
 import Header from "./../../components/layout/Header";
 import Sidebar from "./Sidebar";
 
 export default function UserInformation() {
-    const { loggedinUser } = useContext(UserContext);
+    const { loggedinUser, editUser } = useContext(UserContext);
+    const [formData, setFormData] = useState({
+        username: loggedinUser.username,
+        fullName: loggedinUser.fullName,
+        gender: loggedinUser.gender,
+        email: loggedinUser.email,
+        phoneNumber: loggedinUser.phoneNumber,
+        state: loggedinUser.state,
+        city: loggedinUser.city,
+        zipCode: loggedinUser.zipCode,
+        address: loggedinUser.address,
+    });
+
+    const {
+        username,
+        fullName,
+        gender,
+        email,
+        phoneNumber,
+        state,
+        city,
+        zipCode,
+        address,
+    } = formData;
+
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value,
+        }));
+    };
+
+    const edit = (e) => {
+        e.preventDefault();
+        editUser( formData);
+    };
+
     return (
         <>
             <Header />
@@ -16,71 +52,150 @@ export default function UserInformation() {
                             Your information
                         </div>
                         <div className="user-panel-header-buttons">
-                            <button className="btn btn-second">Edit</button>
+                            <button onClick={edit} className="btn btn-prime">
+                                submit
+                            </button>
                         </div>
                     </div>
                     <div className="user-panel-content">
-                        <div className="user-info-container">
-                            <div className="user-panel-content-user-img">
-                                <img
-                                    src={loggedinUser.imageUrl}
-                                    alt={loggedinUser.username}
-                                />
-                            </div>
-                            <div className="user-panel-content-user-info">
-                                <div className="group-col">
-                                    <div className="data-label">User name</div>
-                                    <div className="data">
-                                        {loggedinUser.username}
-                                    </div>
+                        <form onSubmit={edit}>
+                            <div className="user-info-container">
+                                <div className="user-panel-content-user-img">
+                                    <img
+                                        src={loggedinUser.imageUrl}
+                                        alt={loggedinUser.username}
+                                    />
                                 </div>
-                                <div className="group-col">
-                                    <div className="data-label">Full name</div>
-                                    <div className="data">
-                                        {loggedinUser.fullName}
+                                <div className="user-panel-content-user-info">
+                                    <div className="group-col">
+                                        <div className="data-label">
+                                            User name
+                                        </div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="username"
+                                                value={username}
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="group-col">
-                                    <div className="data-label">Gender</div>
-                                    <div className="data">
-                                        {loggedinUser.gender}
+                                    <div className="group-col">
+                                        <div className="data-label">
+                                            Full name
+                                        </div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="fullName"
+                                                value={fullName}
+                                            />
+                                        </div>
                                     </div>
-                                </div>
+                                    <div className="group-col">
+                                        <div className="data-label">Gender</div>
+                                        <div className="data">
+                                            <select
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                id="gender"
+                                                value={gender}
+                                            >
+                                                <option value="male">
+                                                    male
+                                                </option>
+                                                <option value="female">
+                                                    female
+                                                </option>
+                                            </select>
+                                        </div>
+                                    </div>
 
-                                <div className="group-col">
-                                    <div className="data-label">
-                                        Phone number
+                                    <div className="group-col">
+                                        <div className="data-label">
+                                            Phone number
+                                        </div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="phoneNumber"
+                                                value={phoneNumber}
+                                            />
+                                        </div>
                                     </div>
-                                    <div className="data">
-                                        {loggedinUser.phoneNumber}
+                                    <div className="group-col">
+                                        <div className="data-label">E-mail</div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="email"
+                                                value={email}
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="group-col">
-                                    <div className="data-label">E-mail</div>
-                                    <div className="data">
-                                        {loggedinUser.email}
+                                    <div className="group-col">
+                                        <div className="data-label">State</div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="state"
+                                                value={state}
+                                            />
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="group-col">
-                                    <div className="data-label">State</div>
-                                    <div className="data">{loggedinUser.state}</div>
-                                </div>
-                                <div className="group-col">
-                                    <div className="data-label">City</div>
-                                    <div className="data">{loggedinUser.city}</div>
-                                </div>
-                                <div className="group-col">
-                                    <div className="data-label">Zip code</div>
-                                    <div className="data">{loggedinUser.zipCode}</div>
-                                </div>
-                                <div className="group-col panel-user-info-address">
-                                    <div className="data-label">Address</div>
-                                    <div className="data">
-                                    {loggedinUser.address}
+                                    <div className="group-col">
+                                        <div className="data-label">City</div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="city"
+                                                value={city}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="group-col">
+                                        <div className="data-label">
+                                            Zip code
+                                        </div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="zipCode"
+                                                value={zipCode}
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="group-col panel-user-info-address">
+                                        <div className="data-label">
+                                            Address
+                                        </div>
+                                        <div className="data">
+                                            <input
+                                                className="input-user-info"
+                                                onChange={onChange}
+                                                type="text"
+                                                id="address"
+                                                value={address}
+                                                style={{ width: "85.5%" }}
+                                            />
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
